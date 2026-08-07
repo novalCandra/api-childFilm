4fa16b19063af693bbc632df5579e5eafec426df
novalCandra <dinatanovel5@gmail.com>
2026-08-07 07:27:23 +0700

    feat : ✅ Add API DATA SERIES & AUTH

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = 3000;
-const AuthRouter = require("./src/router/authRouter");
+const AuthRouter = require("./src/router/authRouter.route");
+const SeriesRouter = require("./src/router/seriesRouter.route");
+const upload = require("./src/middleware/UploadPhoto.middleware");
 app.use(express.json())
+// MULTER
+app.use('/assets', express.static("public"))
+app.use(upload.single("image_link"))
 // MIDDLEWARE
 app.get("/", (req, res) => {
     res.send("CHILD FILM API")
 })
 // AUTH
 app.use("/api", AuthRouter)
+app.use("/api", SeriesRouter)
 app.listen(port, () => {
     console.log(`API CHILD BERJALAN PADA PORT : ${port}`)
 })
