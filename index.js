a3d1496fad99c61fa3a219b969de23f9867bdbfe
novalCandra <dinatanovel5@gmail.com>
2026-08-07 20:43:30 +0700

    feat : ✅ Add SUCESS ROUTER GENRE & DAFTAR SAYA (Router)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000;
 const AuthRouter = require("./src/router/authRouter.route");
 const SeriesRouter = require("./src/router/seriesRouter.route");
 const upload = require("./src/middleware/UploadPhoto.middleware");
+const DaftarSayaRouter = require('./src/router/daftarSayaRouter.route');
+const GenreRouter = require('./src/router/genreRouter.route');
 app.use(express.json())
 // MULTER
 app.use('/assets', express.static("public"))
@@ -16,6 +18,8 @@ app.get("/", (req, res) => {
 // AUTH
 app.use("/api", AuthRouter)
 app.use("/api", SeriesRouter)
+app.use("/api", DaftarSayaRouter)
+app.use("/api", GenreRouter)
 app.listen(port, () => {
     console.log(`API CHILD BERJALAN PADA PORT : ${port}`)
 })
