26a5d82b263da74f05650f522c7d59511f3708b5
novalCandra <dinatanovel5@gmail.com>
2026-08-08 15:11:05 +0700

    feat : ⚒️ perubahan connection (Daftar saya)

--- src/model/ModelDaftarSaya.model.js
+++ src/model/ModelDaftarSaya.model.js
@@ -1,17 +1,16 @@
-const pollDbMysql = require("../config/database");
 const getAllModelDaftarSaya = () => {
     const QueryAllDataDaftarSaya = `SELECT daftar_saya.id, users.username, series.name FROM daftar_saya INNER JOIN users ON daftar_saya.id_users=users.id INNER JOIN series ON daftar_saya.id_series = series.id`;
     return pollDbMysql.execute(QueryAllDataDaftarSaya)
 }
 
-const postModelDaftarSaya = (body) => {
+const postModelDaftarSaya = (body, connection) => {
     const QueryPost = `INSERT INTO daftar_saya (id_users, id_series) VALUES('${body.id_users}', '${body.id_series}')`;
-    return pollDbMysql.execute(QueryPost)
+    return connection.execute(QueryPost)
 }
 
-const updateModelDaftarSaya = (id, body) => {
+const updateModelDaftarSaya = (id, body, connection) => {
     const QueryUpdate = `UPDATE daftar_saya SET id_users='${body.id_users}', id_series='${body.id_series}' WHERE id=${id}`;
-    return pollDbMysql.execute(QueryUpdate)
+    return connection.execute(QueryUpdate)
 }
 
 const getDetailUpdate = (id) => {
@@ -19,9 +18,9 @@ const getDetailUpdate = (id) => {
     return pollDbMysql.execute(QueryUpdateDetail)
 }
 
-const DeleteModelDaftarSaya = (id) => {
+const DeleteModelDaftarSaya = (id, connection) => {
     const QueryDelete = `DELETE FROM daftar_saya where id=${id}`
-    return pollDbMysql.execute(QueryDelete)
+    return connection.execute(QueryDelete)
 }
 
 module.exports = {
--- src/service/DaftarSaya.service.js
+++ src/service/DaftarSaya.service.js
@@ -1,17 +1,46 @@
 const { getAllModelDaftarSaya, DeleteModelDaftarSaya, postModelDaftarSaya, updateModelDaftarSaya, getDetailUpdate } = require("../model/ModelDaftarSaya.model");
-
+const pollDbMysql = require("../config/database");
 const GetAllServiceDaftarSaya = async () => {
     const getDataDaftarSaya = await getAllModelDaftarSaya();
     return getDataDaftarSaya
 }
-
-const createServiceDaftarSaya = async(body) => {
-    const createDataDaftarSaya = await postModelDaftarSaya(body)
-    return createDataDaftarSaya
+const createServiceDaftarSaya = async (body) => {
+    let connection;
+    try {
+        connection = await pollDbMysql.getConnection();
+        await connection.beginTransaction();
+        const [result] = await postModelDaftarSaya(body, connection)
+        await connection.commit();
+        return [result]
+    } catch (error) {
+        if (connection) {
+            await connection.rollback()
+        }
+        throw error
+    } finally {
+        if (connection) {
+            connection.release()
+        }
+    }
 }
-const updateServiceDaftarSaya = async(id, body) => {
-    const updateDataDaftarSaya = await updateModelDaftarSaya(id,body)
-    return updateDataDaftarSaya
+const updateServiceDaftarSaya = async (id, body) => {
+    let connection
+    try {
+        connection = await pollDbMysql.getConnection();
+        await connection.beginTransaction();
+        const [result] = await updateModelDaftarSaya(id, body, connection)
+        await connection.commit();
+        return result
+    } catch (error) {
+        if (connection) {
+            await connection.rollback()
+        }
+        throw error
+    } finally {
+        if (connection) {
+            connection.release()
+        }
+    }
 }
 
 const updateSelectServiceDaftarSaya = async (id) => {
@@ -20,8 +49,23 @@ const updateSelectServiceDaftarSaya = async (id) => {
 }
 
 const deleteServiceDaftarSaya = async (id) => {
-    const deleteDataDaftarSaya = await DeleteModelDaftarSaya(id)
-    return deleteDataDaftarSaya
+    let connection;
+    try {
+        connection = await pollDbMysql.getConnection();
+        await connection.beginTransaction();
+        const [result] = await DeleteModelDaftarSaya(id, connection)
+        await connection.commit();
+        return result
+    } catch (error) {
+        if (connection) {
+            connection.callback()
+        }
+        throw error
+    } finally {
+        if (connection) {
+            connection.release()
+        }
+    }
 }
 module.exports = {
     GetAllServiceDaftarSaya,
