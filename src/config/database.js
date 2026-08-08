c447a91ac2aee0b7c660b76c449dc9a8ee7972d2
novalCandra <dinatanovel5@gmail.com>
2026-08-08 15:08:55 +0700

    feat : ✅ perubahan databae (mysql2)

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
-const mysql2 = require("mysql2");
+const mysql2 = require("mysql2/promise");
 
-const pollDB = mysql2.createConnection({
+const pollDB = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 })
 
-module.exports = pollDB.promise();
+module.exports = pollDB;
